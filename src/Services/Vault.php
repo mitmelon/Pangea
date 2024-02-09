d379b0d9e2825d736cd8855a09b729257685aec8
Adeyeye George <manomitehq@gmail.com>
2024-02-09 14:29:47 +0100

    updated

--- src/Services/Vault.php
+++ src/Services/Vault.php
@@ -16,10 +16,11 @@ class Vault implements PangeaInterface {
     private $allowed_state = ["active", "deactivated", "destroyed", "inherited", "suspended", "compromised"];
     private $asymmetric_algo = ["ED25519", "RSA-PKCS1V15-2048-SHA256", "ES256", "ES384", "ES512", "ES256K", "RSA-PSS-2048-SHA256", "RSA-PSS-3072-SHA256", "RSA-PSS-4096-SHA256", "RSA-PSS-4096-SHA512", "ED25519-DILITHIUM2", "ED448-DILITHIUM3", "SPHINCSPLUS-128-SHAKE256-SIMPLE", "SPHINCSPLUS-128-SHAKE256-ROBUST", "SPHINCSPLUS-128-SHA256-SIMPLE", "SPHINCSPLUS-128-SHA256-ROBUST", "SPHINCSPLUS-192-SHAKE256-SIMPLE", "SPHINCSPLUS-192-SHAKE256-ROBUST", "SPHINCSPLUS-192-SHA256-SIMPLE", "SPHINCSPLUS-192-SHA256-ROBUST", "SPHINCSPLUS-256-SHAKE256-SIMPLE", "SPHINCSPLUS-256-SHAKE256-ROBUST", "SPHINCSPLUS-256-SHA256-SIMPLE", "SPHINCSPLUS-256-SHA256-ROBUST", "RSA-OAEP-2048-SHA1", "RSA-OAEP-2048-SHA256", "RSA-OAEP-2048-SHA512", "RSA-OAEP-3072-SHA1", "RSA-OAEP-3072-SHA256", "RSA-OAEP-3072-SHA512", "RSA-OAEP-4096-SHA1", "RSA-OAEP-4096-SHA256", "RSA-OAEP-4096-SHA512", "ES256", "ES384", "ES512"];
     private $symmetric_algo_jwt = ["AES-CFB-128", "AES-CFB-256", "AES-GCM-256", "AES-CBC-128", "AES-CBC-256", "HS256", "HS384", "HS512"];
+
     private $symmetric_algo_encryption = ["AES-CFB-128", "AES-CFB-256", "AES-GCM-256", "AES-CBC-128", "AES-CBC-256"];
 
-   
     protected $travel;
+
     protected $version;
 
     public function setParentProperties(\Pangea\Pangea $parent){
@@ -32,23 +33,36 @@ class Vault implements PangeaInterface {
             throw new \Exception('Invalid key type. Please choose from the allowed key types ' . implode(', ', $this->allowed_type));
         }
 
+        if (!in_array(strtolower($purpose), $this->allowed_purpose)) {
+            throw new \Exception('Invalid purpose type. Please choose from the allowed purpose types ' . implode(', ', $this->allowed_purpose));
+        }
+
         $algo = str_replace('_key', '', $type).'_algo_'.$purpose;
         if (!in_array($algorithm, $this->$algo)) {
             throw new \Exception('Invalid algorithm. Please choose from the allowed types ' . implode(', ', $this->$algo));
         }
 
-        if (!in_array(strtolower($purpose), $this->allowed_purpose)) {
-            throw new \Exception('Invalid purpose type. Please choose from the allowed purpose types ' . implode(', ', $this->allowed_purpose));
-        }
+
         if (!in_array(strtolower($rotation_state), $this->allowed_state)) {
             throw new \Exception('Invalid rotation state. Please choose from the allowed state types ' . implode(', ', $this->allowed_state));
         }
         $expire = array();
         if(!empty($expiration)){
-            $datetime = \DateTime::createFromFormat('Y-m-d\TH:i:s+', $expiration);
-            $expire = array('expiration' => $datetime);
+            $expire = array('expiration' => $expiration);
         }
-        
+
+        print_r(array_merge([
+            'type' => $type,
+            'algorithm' => $algorithm,
+            'purpose' => $purpose,
+            'name' => $keyName,
+            'folder' => $folderName,
+            'metadata' => $metadata,
+            'tags' => $tags,
+            'rotation_frequency' => $rotation_frequency,
+            'rotation_state' => $rotation_state,
+        ], $expire));
+
         $response = $this->travel->post('/'.$this->version.'/key/generate', array_merge([
             'type' => $type,
             'algorithm' => $algorithm,
@@ -59,7 +73,6 @@ class Vault implements PangeaInterface {
             'tags' => $tags,
             'rotation_frequency' => $rotation_frequency,
             'rotation_state' => $rotation_state,
-            
         ], $expire));
         return $response;
     }
@@ -70,32 +83,30 @@ class Vault implements PangeaInterface {
         }
         $response = $this->travel->post('/'.$this->version.'/key/rotate', [
             'id' => $id,
-            'rotation_state' => $rotation_state,
-            'raw' => true,
-            'verbose' => true
+            'rotation_state' => $rotation_state
         ]);
         return $response;
     }
 
-    public function encrypt($id, string $text, string $additional_data = null){
+    public function encrypt($id, string $text, string $additional_data = ''){
         $response = $this->travel->post('/'.$this->version.'/key/encrypt', [
             'id' => $id,
-            'plain_text' => $text,
+            'plain_text' => base64_encode($text),
             'additional_data' => $additional_data,
-            'raw' => true,
-            'verbose' => true
         ]);
         return $response;
     }
 
-    public function decrypt($id, string $cipher, string $additional_data = null){
+    public function decrypt($id, string $cipher, string $additional_data = ''){
         $response = $this->travel->post('/'.$this->version.'/key/decrypt', [
             'id' => $id,
             'cipher_text' => $cipher,
             'additional_data' => $additional_data,
-            'raw' => true,
-            'verbose' => true
         ]);
+        if($response['status'] === 'Success'){
+            $response['result']['plain_text'] = base64_decode($response['result']['plain_text']);
+            return $response;
+        }
         return $response;
     }
 
@@ -103,8 +114,6 @@ class Vault implements PangeaInterface {
         $response = $this->travel->post('/'.$this->version.'/key/sign', [
             'id' => $id,
             'message' => $message,
-            'raw' => true,
-            'verbose' => true
         ]);
         return $response;
     }
@@ -114,13 +123,11 @@ class Vault implements PangeaInterface {
             'id' => $id,
             'message' => $message,
             'signature' => $signature,
-            'raw' => true,
-            'verbose' => true
         ]);
         return $response;
     }
 
-    public function store(string $type, string $public_key, $private_key, string $purpose = "signing", string $keyName = null, string $folderName = null, array $metadata = array(), string | array $tags = null, string $rotation_frequency = '10d', string $rotation_state = 'inherited', string $expiration = null){
+    public function store(string $type, string $algorithm, string $public_key, $private_key, string $purpose = "encryption", string $keyName = '', string $folderName = '', object $metadata = new \stdClass(), array $tags = array(), string $rotation_frequency = '10d', string $rotation_state = 'inherited', string $expiration = ''){
        
         if (!in_array(strtolower($type), $this->allowed_type)) {
             throw new \Exception('Invalid key type. Please choose from the allowed key types ' . implode(', ', $this->allowed_type));
@@ -128,11 +135,22 @@ class Vault implements PangeaInterface {
         if (!in_array(strtolower($purpose), $this->allowed_purpose)) {
             throw new \Exception('Invalid purpose type. Please choose from the allowed purpose types ' . implode(', ', $this->allowed_purpose));
         }
+
+        $algo = str_replace('_key', '', $type).'_algo_'.$purpose;
+        if (!in_array($algorithm, $this->$algo)) {
+            throw new \Exception('Invalid algorithm. Please choose from the allowed types ' . implode(', ', $this->$algo));
+        }
+
         if (!in_array(strtolower($rotation_state), $this->allowed_state)) {
             throw new \Exception('Invalid rotation state. Please choose from the allowed state types ' . implode(', ', $this->allowed_state));
         }
-        $response = $this->travel->post('/'.$this->version.'/key/store', [
+        $expire = array();
+        if(!empty($expiration)){
+            $expire = array('expiration' => $expiration);
+        }
+        $response = $this->travel->post('/'.$this->version.'/key/store', array_merge([
             'type' => $type,
+            'algorithm' => $algorithm,
             'purpose' => $purpose,
             'public_key' => $public_key,
             'private_key' => $private_key,
@@ -143,24 +161,24 @@ class Vault implements PangeaInterface {
             'rotation_frequency' => $rotation_frequency,
             'rotation_state' => $rotation_state,
             'expiration' => $expiration,
-            'raw' => true,
-            'verbose' => true
-        ]);
+        ], $expire));
         return $response;
     }
 
-    public function createFolder($folderName, string $path, array $metadata = null, string | array $tags = null, string $rotation_frequency = '10d', string $rotation_state = 'inherited', string $expiration = null){
-        $response = $this->travel->post('/'.$this->version.'/folder/create', [
+    public function createFolder($folderName, string $path, object $metadata = new \stdClass(), array $tags = array(), string $rotation_frequency = '10d', string $rotation_state = 'inherited', string $expiration = ''){
+        $expire = array();
+        if(!empty($expiration)){
+            $expire = array('expiration' => $expiration);
+        }
+        $response = $this->travel->post('/'.$this->version.'/folder/create', array_merge([
             'name' => $folderName,
             'folder' => $path,
             'metadata' => $metadata,
             'tags' => $tags,
             'rotation_frequency' => $rotation_frequency,
             'rotation_state' => $rotation_state,
-            'expiration' => $expiration,
-            'raw' => true,
-            'verbose' => true
-        ]);
+            'expiration' => $expiration
+        ], $expire));
         return $response;
     }
 
@@ -168,8 +186,6 @@ class Vault implements PangeaInterface {
         $response = $this->travel->post('/'.$this->version.'/key/sign/jwt', [
             'id' => $id,
             'payload' => $payload,
-            'raw' => true,
-            'verbose' => true
         ]);
         return $response;
     }
@@ -177,8 +193,6 @@ class Vault implements PangeaInterface {
     public function verifyJWT($jws){
         $response = $this->travel->post('/'.$this->version.'/key/verify/jwt', [
             'jws' => $jws,
-            'raw' => true,
-            'verbose' => true
         ]);
         return $response;
     }
@@ -190,7 +204,7 @@ class Vault implements PangeaInterface {
         return $response;
     }
 
-    public function rotateSecret($id, $secret = null){
+    public function rotateSecret($id, $secret = ''){
         $response = $this->travel->post('/'.$this->version.'/key/secret/rotate', [
             'id' => $id,
             'secret' => $secret,
@@ -200,7 +214,7 @@ class Vault implements PangeaInterface {
         return $response;
     }
 
-    public function storeSecret($type, $secret = null, string $name = null, string $folderPath = null, array $metadata = array(), string | array $tags = null, string $rotation_frequency = '10d', string $rotation_state = 'inherited', string $rotation_grace_period = '7d', string $expiration = null){
+    public function storeSecret($type, $secret = '', string $name = '', string $folderPath = '', object $metadata = new \stdClass(), array $tags = array(), string $rotation_frequency = '10d', string $rotation_state = 'inherited', string $rotation_grace_period = '7d', string $expiration = ''){
 
         $allowed_type = ["secret", "pangea_token"];
 
@@ -212,7 +226,12 @@ class Vault implements PangeaInterface {
             throw new \Exception('Invalid rotation state. Please choose from the allowed state types ' . implode(', ', $this->allowed_state));
         }
 
-        $response = $this->travel->post('/'.$this->version.'/key/secret/store', [
+        $expire = array();
+        if(!empty($expiration)){
+            $expire = array('expiration' => $expiration);
+        }
+
+        $response = $this->travel->post('/'.$this->version.'/key/secret/store', array_merge([
             'type' => $type,
             'secret' => $secret,
             'name' => $name,
@@ -221,16 +240,14 @@ class Vault implements PangeaInterface {
             'tags' => $tags,
             'rotation_frequency' => $rotation_frequency,
             'rotation_state' => $rotation_state,
-            'expiration' => $expiration,
-            'raw' => true,
-            'verbose' => true
-        ]);
+            'expiration' => $expiration
+        ], $expire));
         return $response;
     }
 
 
     //Retrieve a secret, key or folder, and any associated information.
-    public function getKey($id, $secret = null){
+    public function getKey($id, $secret = ''){
         $response = $this->travel->post('/'.$this->version.'/get', [
             'id' => $id,
             'raw' => true,
@@ -239,7 +256,7 @@ class Vault implements PangeaInterface {
         return $response;
     }
 
-    public function listKey(array $filter, int $size = 20, string $order = 'asc', string $order_by = 'name', string $lastID = null, bool $include_secrets = false, array | string $include = null){
+    public function listKey(array $filter, int $size = 20, string $order = 'asc', string $order_by = 'name', string $lastID = '', bool $include_secrets = false, array | string $include = ''){
 
         $order_type = ["asc", "desc"];
         $order_by_type = ["id", "type", "created_at", "algorithm", "purpose", "expiration", "last_rotated", "next_rotation", "name", "folder", "item_state"];
@@ -264,7 +281,7 @@ class Vault implements PangeaInterface {
     }
 
     //Update information associated with a secret, key or folder.
-    public function updateSecret(string $id, string $name = null, string $folderPath = null, array $metadata = array(), string | array $tags = null, string $rotation_frequency = '10d', string $rotation_state = 'inherited', string $rotation_grace_period = '7d', string $expiration = null, string $item_state = 'enabled'){
+    public function updateSecret(string $id, string $name = '', string $folderPath = '', object $metadata = new \stdClass(), array $tags = array(), string $rotation_frequency = '10d', string $rotation_state = 'inherited', string $rotation_grace_period = '7d', string $expiration = '', string $item_state = 'enabled'){
 
         $allowed_item_type = ["enabled", "disabled"];
 
@@ -272,7 +289,12 @@ class Vault implements PangeaInterface {
             throw new \Exception('Invalid allowed type. Please choose from the allowed types ' . implode(', ', $allowed_item_type));
         }
 
-        $response = $this->travel->post('/'.$this->version.'/update', [
+        $expire = array();
+        if(!empty($expiration)){
+            $expire = array('expiration' => $expiration);
+        }
+
+        $response = $this->travel->post('/'.$this->version.'/update', array_merge([
             'id' => $id,
             'name' => $name,
             'folder' => $folderPath,
@@ -281,10 +303,8 @@ class Vault implements PangeaInterface {
             'rotation_frequency' => $rotation_frequency,
             'rotation_state' => $rotation_state,
             'expiration' => $expiration,
-            'item_state' => $item_state,
-            'raw' => true,
-            'verbose' => true
-        ]);
+            'item_state' => $item_state
+        ], $expire));
         return $response;
     }
 
